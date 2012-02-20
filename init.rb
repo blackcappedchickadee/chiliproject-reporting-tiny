4b86590a5bc5787f806bf578918d06ea8dc96d8c
Chirpmaster Cloudchirp <cloudchirp.chirpmaster@gmail.com>
2012-02-19 19:57:36 -0500

    Initial commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :chiliproject_reporting_tiny do
+  name 'Chiliproject Reporting Tiny plugin'
+  author 'Cloudchirp'
+  description 'This is a plugin for ChiliProject'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
